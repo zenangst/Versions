5b2fb6d9562ddb11011a55be21adf4c49dda2918
Konstantin Koval <konstantin@rocketfarm.no>
2015-03-06 17:08:33 +0100

    Use switch, not a big win. + add gitignore

--- Source/Versions.swift
+++ Source/Versions.swift
@@ -29,26 +29,25 @@ public extension String {
     func newerThan(version :String) -> Bool {
         return self.compare(version, options: NSStringCompareOptions.NumericSearch) == NSComparisonResult.OrderedDescending
     }
-    
+
     func olderThan(version: String) -> Bool {
         let isEqual: Bool = self == version
         return !isEqual ? !self.newerThan(version) : false
     }
 
     func semanticCompare(version: String) -> Semantic {
-        var semanticType = Semantic.Unknown
-
-        if self == version {
-            semanticType = Semantic.Same
-        } else if self[0] != version[advance(self.startIndex, 0)] {
-            semanticType = Semantic.Major
-        } else if self[0...2] != version[0...2] && self.olderThan(version) {
-            semanticType = Semantic.Minor
-        } else if self[0...4] != version[0...4] && self.olderThan(version) {
-            semanticType = Semantic.Patch
+        switch version {
+        case let version where self == version:
+            return .Same
+        case let version where self[0] != version[self.startIndex]:
+            return .Major
+        case let version where self[0...2] != version[0...2] && self.olderThan(version):
+            return .Minor
+        case let version where self[0...4] != version[0...4] && self.olderThan(version):
+            return .Patch
+        default:
+            return .Unknown
         }
-
-        return semanticType
     }
-    
+
 }
