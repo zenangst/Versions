904f4656f47732f72e247da05a317526f47dcddb
Christoffer Winterkvist <chris@zenangst.com>
2015-05-20 19:15:29 +0200

    Update Demo

--- VersionsDemo/ViewController.swift
+++ VersionsDemo/ViewController.swift
@@ -10,16 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var version: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        version.text = App.version
     }
-
 
 }
 
