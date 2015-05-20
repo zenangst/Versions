892b1ef110c59e4c03fb0aa6542ac26ab6caee78
Christoffer Winterkvist <chris@zenangst.com>
2015-05-20 16:52:28 +0200

    Remove overhead

--- VersionsDemo/AppDelegate.swift
+++ VersionsDemo/AppDelegate.swift
@@ -12,12 +12,12 @@ import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+  var window: UIWindow?
 
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    println(App.version)
+    return true
+  }
 }
 
